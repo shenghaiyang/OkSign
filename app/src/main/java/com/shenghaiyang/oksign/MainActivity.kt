13262aa939554433adf0a216301ed4e9734bc39c
shenghaiyang <shenghaiyang@live.cn>
2024-01-21 22:07:14 +0800

    Migrate to enableEdgeToEdge api

--- app/src/main/java/com/shenghaiyang/oksign/MainActivity.kt
+++ app/src/main/java/com/shenghaiyang/oksign/MainActivity.kt
@@ -3,34 +3,16 @@ package com.shenghaiyang.oksign
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.runtime.DisposableEffect
-import androidx.compose.ui.graphics.Color
-import androidx.core.view.WindowCompat
-import com.google.accompanist.systemuicontroller.rememberSystemUiController
+import androidx.activity.enableEdgeToEdge
 import com.shenghaiyang.oksign.ui.App
 import com.shenghaiyang.oksign.ui.theme.OkSignTheme
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
-        WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {
-            val systemUiController = rememberSystemUiController()
-            val useDarkIcons = !isSystemInDarkTheme()
-            DisposableEffect(systemUiController, useDarkIcons) {
-                // Update all of the system bar colors to be transparent, and use
-                // dark icons if we're in light theme
-                systemUiController.setSystemBarsColor(
-                    color = Color.Transparent,
-                    darkIcons = useDarkIcons
-                )
-
-                // setStatusBarColor() and setNavigationBarColor() also exist
-
-                onDispose {}
-            }
             OkSignTheme {
                 App()
             }
